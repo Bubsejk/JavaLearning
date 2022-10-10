6ee7af3a6af23ee6a2207418886385f8f535186f
Daniel Bułanowski <73350075+Bubsejk@users.noreply.github.com>
2022-10-10 21:11:07 +0200

    Add files via upload

---
+++ JavaWhileLoops/JavaForLoops/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Make Loops!");
+        for (int i = 0; i < 5 ; i++){
+            System.out.println("Loop " + i );
+        }
+    }
+}
